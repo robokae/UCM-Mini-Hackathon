15673bf872f5f2348864a4383e707f361c04326e
Yash Sharma <mr.sharmayash@outlook.com>
2019-12-10 19:18:07 -0800

    Update account.js

--- Web Application/backend/account.js
+++ Web Application/backend/account.js
@@ -4,16 +4,7 @@ function loadVars(){
     
 }
 
-const firebaseConfig = {
-    apiKey: "AIzaSyCzM5btiHZBnFWXpcWRfe0fD7J_6uLdoQI",
-    authDomain: "eevent-10adf.firebaseapp.com",
-    databaseURL: "https://eevent-10adf.firebaseio.com",
-    projectId: "eevent-10adf",
-    storageBucket: "eevent-10adf.appspot.com",
-    messagingSenderId: "691801299872",
-    appId: "1:691801299872:web:22c330ae3b6c13f411a2c5",
-    measurementId: "G-P2G2BLQNRK"
-  };
+
   //initialize firebase
   firebase.initializeApp(firebaseConfig);
   
